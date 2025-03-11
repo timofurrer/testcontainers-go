91bbf1c2cf1cbe78275b96317f2acb4b95ae4d7a
Timo Furrer <tuxtimo@gmail.com>
2025-03-11 12:21:01 +0100

    WIP

--- docker.go
+++ docker.go
@@ -1180,7 +1180,7 @@ func (p *DockerProvider) CreateContainer(ctx context.Context, req ContainerReque
 		// a container lifecycle hook will be added, which will expose the host ports to the container
 		// using a SSHD server running in a container. The SSHD server will be started and will
 		// forward the host ports to the container ports.
-		sshdForwardPortsHook, err := exposeHostPorts(ctx, &req, req.HostAccessPorts...)
+		sshdForwardPortsHook, err := exposeHostPorts(ctx, &req, p.hostCache, req.HostAccessPorts...)
 		if err != nil {
 			return nil, fmt.Errorf("expose host ports: %w", err)
 		}
